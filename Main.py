24f3317a976e4db7c9547e3a2044d8637b5d84a4
Adam-Nichols <adakarlnichols@live.co.uk>
2019-10-01 13:03:37 +0100

    Added a simple tkinter gui that will be reworked to fit this script.

--- Main.py
+++ Main.py
@@ -1,10 +1,33 @@
-import sympy
+from sympy import diff, Symbol
+import tkinter as tk
 
-function = input("Please enter the fucntion you would like to differentiate: ")
-derivative_variable = sympy.Symbol(input("Which function would you like to differentiate with respect to? "))
 
-first_derivative = sympy.diff(function, derivative_variable)
+class MainGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Derivative Calculaor")
+
+        self.label = tk.Label(master, text="This program calculates derivatives of functions")
+        self.label.pack()
+
+        self.greet_button = tk.Button(master, text='Greet', command=self.greet)
+        self.greet_button.pack()
+
+        self.close_button = tk.Button(master, text='Close', command=master.quit)
+        self.close_button.pack()
+
+    def greet(self):
+        print("Greetings")
+
+
+root = tk.Tk()
+my_gui = MainGUI(root)
+root.mainloop()
+
+function = input("Please enter the function you would like to differentiate: ")
+derivative_variable = Symbol(input("Which function would you like to differentiate with respect to? "))
+
+first_derivative = diff(function, derivative_variable)
 first_derivative.doit()
 
 print(first_derivative)
-
